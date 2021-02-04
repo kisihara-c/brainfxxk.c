e24eeef2c1201d433cefa1d7ce14c93b4da8dc77
kisihara_c <kisihara.c@gmail.com>
2021-02-05 00:47:32 +0900

    form'sWireframe

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
+let count = 4;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrainFxxkBox />
   );
+}
+
+
+class BrainFxxkBox extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      lt: "",
+      gt: "",
+    };
+  }
+
+  handleChange(event) {
+  }
+
+  render() {
+    return (
+      <form>
+
+        <label>
+          brfk:
+          <input type="text" name="brfk" value={this.state.brfk} onChange={this.handleChange} />
+        </label>
+        <br />
+
+        <label>
+          &lt; :
+          <input type="text" name="lt" value={this.state.lt} onChange={this.handleChange} />
+        </label>
+        <label>
+          &gt;:
+          <input type="text" name="gt" value={this.state.gt} onChange={this.handleChange} />
+        </label>
+        <br />
+        <label>
+          +:
+          <input type="text" name="plus" value={this.state.plus} onChange={this.handleChange} />
+        </label>
+        <label>
+          -:
+          <input type="text" name="minus" value={this.state.minus} onChange={this.handleChange} />
+        </label>
+        <br />
+        <label>
+          .:
+          <input type="text" name="dot" value={this.state.dot} onChange={this.handleChange} />
+        </label>
+        <label>
+          ,:
+          <input type="text" name="conma" value={this.state.conma} onChange={this.handleChange} />
+        </label>
+        <br />
+        <label>
+          [:
+          <input type="text" name="leftPrts" value={this.state.leftPrts} onChange={this.handleChange} />
+        </label>
+        <label>
+          ]:
+          <input type="text" name="rightPrts" value={this.state.rightPrts} onChange={this.handleChange} />
+        </label>
+      </form>
+    );
+  }
 }
 
 export default App;
