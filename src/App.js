2e71c7f7f21e5998441ba0eff09aa96a569ef2a6
kisihara_c <kisihara.c@gmail.com>
2021-02-07 14:50:09 +0900

    MakingBone:inputBrfk,returnSameWordsInNew

--- src/App.js
+++ src/App.js
@@ -13,12 +13,25 @@ class BrainFxxkBox extends React.Component{
   constructor(props) {
     super(props);
     this.state = {
-      lt: "",
-      gt: "",
+      brfk: "",
+      newCode: ""
     };
   }
 
-  handleChange(event) {
+  handleChange = (event) => {
+    if(event.target.name==="brfk"){
+      this.setState({newCode:this.codeConvert(event.target.value)})
+      this.setState({[event.target.name]: event.target.value});
+    }
+    else if(event.target.name==="newCode"){
+    }
+    else{
+      this.setState({[event.target.name]: event.target.value});
+    }
+  }
+
+  codeConvert = (s) =>{
+    return s;
   }
 
   render() {
@@ -27,46 +40,55 @@ class BrainFxxkBox extends React.Component{
 
         <label>
           brfk:
-          <input type="text" name="brfk" value={this.state.brfk} onChange={this.handleChange} />
+            <input type="text" name="brfk" value={this.state.brfk} onChange={this.handleChange} />
         </label>
+
+        <label>
+          new:
+            <input type="text" name="newCode" value={this.state.newCode} onChange={this.handleChange} />
+        </label>
+
+        {/*
         <br />
 
         <label>
           &lt; :
-          <input type="text" name="lt" value={this.state.lt} onChange={this.handleChange} />
+          <input type="text" name="lt" value={lt} onChange={this.handleChange} />
         </label>
         <label>
           &gt;:
-          <input type="text" name="gt" value={this.state.gt} onChange={this.handleChange} />
+          <input type="text" name="gt" value={gt} onChange={this.handleChange} />
         </label>
         <br />
         <label>
           +:
-          <input type="text" name="plus" value={this.state.plus} onChange={this.handleChange} />
+          <input type="text" name="plus" value={plus} onChange={this.handleChange} />
         </label>
         <label>
           -:
-          <input type="text" name="minus" value={this.state.minus} onChange={this.handleChange} />
+          <input type="text" name="minus" value={minus} onChange={this.handleChange} />
         </label>
         <br />
         <label>
           .:
-          <input type="text" name="dot" value={this.state.dot} onChange={this.handleChange} />
+          <input type="text" name="dot" value={dot} onChange={this.handleChange} />
         </label>
         <label>
           ,:
-          <input type="text" name="conma" value={this.state.conma} onChange={this.handleChange} />
+          <input type="text" name="conma" value={conma} onChange={this.handleChange} />
         </label>
         <br />
         <label>
           [:
-          <input type="text" name="leftPrts" value={this.state.leftPrts} onChange={this.handleChange} />
+          <input type="text" name="leftPrts" value={leftPrts} onChange={this.handleChange} />
         </label>
         <label>
           ]:
-          <input type="text" name="rightPrts" value={this.state.rightPrts} onChange={this.handleChange} />
+          <input type="text" name="rightPrts" value={rightPrts} onChange={this.handleChange} />
         </label>
+      */}
       </form>
+      
     );
   }
 }
